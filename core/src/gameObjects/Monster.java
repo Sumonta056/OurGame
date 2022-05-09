6962be17b47438967c6c653074efd3196169e2c4
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2022-05-09 07:47:04 +0600

    Monster Screen Added
    1- Random monster for air
    2 -Random monster for water
    3 - update Game screen

---
+++ core/src/gameObjects/Monster.java
@@ -0,0 +1,123 @@
+package gameObjects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import extra.Collision;
+
+import java.util.Random;
+
+public class Monster implements ForObject{
+    Collision collision; // collision object for checking with any other objects
+    Random rand; // for creating random value
+
+
+    public float monsterX = Gdx.graphics.getWidth(); // x-axis of monsters
+    public float monsterY; // y-axis of monsters (randomly created in constructor)
+    public float monsterVerticalSpeed = 60f; // vertical speed of monster
+    public float monsterHorizontalSpeed = 250f; // horizontal speed of monster
+    public float monsterWidth = 120f; // to define monster width
+    public float monsterHeight = 56.64f; // to define monster height
+    public float airMonsterSafeDistance = 20f; // air monsters safe distance from water
+    public float waterMonsterSafeDistance = 10f; // water monsters safe distance from air
+    public float waterMonsterMinDistance = 0f; // min distance of water monsters. (the lowest point they can go)
+    public float waterMonsterMaxDistance = Gdx.graphics.getHeight() / 2f - monsterHeight; // max distance of
+    // water monsters. (up to which they can go)
+    public float airMonsterMinDistance = Gdx.graphics.getHeight() / 2f; // min distance of air monsters.
+    public float airMonsterMaxDistance = Gdx.graphics.getHeight(); // max distance of air monsters
+    public float monsterDirection = 1f; // direction of water monsters. if they are going upward or downward.
+    public int waterMonsterCnt = 4; // count of water monsters
+    public int airMonsterCnt = 4; // count of air monsters
+    public String[] monsterImgLoc = {"Monster\\mons1.png", "Monster\\mons2.png", "Monster\\mons3.png",
+            "Monster\\mons4.png", "Monster\\mons5.png", "Monster\\mons6.png",
+            "Monster\\mons7.png", "Monster\\mons8.png"}; // image location of monsters (water & air)
+    Texture monsTexture; // texture for monster images
+    public boolean remove = false; // a boolean value to remove monsters after collision with character
+    // or when x-axis < 0
+
+
+    /*
+    default constructor
+     */
+    public Monster() {
+        rand = new Random(); // initializing random object
+
+        monsterY = rand.nextFloat() * (Gdx.graphics.getHeight() - monsterHeight); // creating random y-axis
+
+        selectMonsterTexture(); // for selecting random monster texture.
+
+        collision = new Collision(monsterX, monsterY, monsterWidth, monsterHeight); // creating collision object
+    }
+
+    /*
+    updates monster's x and y-axis
+     */
+    @Override
+    public void update() {
+        // changing directing for water monsters.
+        if (monsterY >= waterMonsterMaxDistance - waterMonsterSafeDistance &&
+                monsterY <= waterMonsterMaxDistance) {
+            monsterDirection *= -1;
+            monsterY = waterMonsterMaxDistance - waterMonsterSafeDistance;
+        }
+        else if (monsterY <= waterMonsterMinDistance) {
+            monsterDirection *= -1;
+            monsterY = waterMonsterMinDistance;
+        }
+
+        // changing monster y-axis for water monsters
+        if (monsterY <= waterMonsterMaxDistance) {
+            monsterY += monsterDirection * monsterVerticalSpeed * Gdx.graphics.getDeltaTime();
+        }
+
+        monsterX -= monsterHorizontalSpeed * Gdx.graphics.getDeltaTime(); // changing x-axis for all monsters.
+
+        // if monster is out of the screen it should be removed.
+        if (monsterX + monsterWidth <= 0) {
+            remove = true;
+        }
+
+        collision.update(monsterX, monsterY, monsterWidth, monsterHeight); // updating collision object.
+    }
+
+    /*
+    used to draw monsters.
+     */
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(monsTexture, monsterX, monsterY, monsterWidth, monsterHeight); // drawing monster.
+    }
+
+    /*
+    used to check if the monster is colliding with any other object
+     */
+    @Override
+    public Collision getCollision() {
+        return collision;
+    }
+
+    /*
+    selecting random monster according to y-axis.
+    if y-axis is in the air, the monster will be air monster.
+    if y-axis is in the water, the monster will be water monster.
+     */
+    public void selectMonsterTexture() {
+        if (monsterY > airMonsterMinDistance &&
+                monsterY <= airMonsterMinDistance + airMonsterSafeDistance) {
+            monsterY = airMonsterMinDistance + airMonsterSafeDistance;
+        }
+        else if (monsterY > waterMonsterMaxDistance - waterMonsterSafeDistance &&
+                monsterY <= waterMonsterMaxDistance + monsterHeight) {
+            monsterY = waterMonsterMaxDistance - waterMonsterSafeDistance;
+        }
+
+        int idx;
+        // if the condition is true. the monster is in the water else in air.
+        if (monsterY <= waterMonsterMaxDistance) idx = rand.nextInt(waterMonsterCnt) + airMonsterCnt;
+        else idx = rand.nextInt(airMonsterCnt); // index from 0 to airMonsterCnt - 1.
+
+        if (idx == 0) monsterWidth = 150.75f; // to adjust width of first monster
+
+        monsTexture = new Texture(monsterImgLoc[idx]); // taking texture according to index.
+    }
+}
--- core/src/screen/GameScreen.java
+++ core/src/screen/GameScreen.java
@@ -1,20 +1,36 @@
 package screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.our.game.MainGame;
 import gameObjects.Character;
+import gameObjects.Monster;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameScreen implements Screen {
 
     MainGame game;
 
     Character character; // character object
+    Random rand; // for taking random value
+
+    // monster starts
+    ArrayList<Monster> monsters;
+    public float monsterMinLaunchTime = 1f;
+    public float monsterMaxLaunchTime = 4f;
+    public float monsterLaunchTime = 0f;
+    // monster ends
+
 
     public GameScreen(MainGame game) {
         this.game = game;
+        rand = new Random();
 
         character = new Character(); // creating character object
+        monsters = new ArrayList<>(); // for storing Monster objects
     }
 
     @Override
@@ -40,11 +56,34 @@ public class GameScreen implements Screen {
     this method will be used to update all objects
      */
     public void updateObjects() {
+
         updateCharacter();
+
+        updateMonster();
+
     }
 
     public void updateCharacter() {
+
         character.update();
+    }
+
+    public void updateMonster() {
+
+        monsterLaunchTime -= Gdx.graphics.getDeltaTime();
+        if (monsterLaunchTime <= 0) {
+            monsterLaunchTime = rand.nextFloat() * (monsterMaxLaunchTime - monsterMinLaunchTime) + monsterMinLaunchTime;
+            monsters.add(new Monster());
+        }
+
+        ArrayList<Monster> monsterToRemove = new ArrayList<>();
+        for (Monster monster : monsters) {
+            monster.update();
+            if (monster.remove) {
+                monsterToRemove.add(monster);
+            }
+        }
+        monsters.removeAll(monsterToRemove);
     }
 
     /*
@@ -61,6 +100,9 @@ public class GameScreen implements Screen {
         renderBackground();
 
         renderCharacter();
+
+        renderMonster();
+
     }
 
     public void renderBackground() {
@@ -69,6 +111,12 @@ public class GameScreen implements Screen {
 
     public void renderCharacter() {
         character.render(game.batch);
+    }
+
+    public void renderMonster() {
+        for (Monster monster : monsters) {
+            monster.render(game.batch);
+        }
     }
 
     @Override
